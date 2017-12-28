63df8f8462b8bf1a61cdbb2bb440819372b8656a
mars13code <34914452+mars13code@users.noreply.github.com>
2017-12-28 14:14:27 +0100

    charger le fichier style.css du thème parent
    La méthode recommandée par WP

--- functions.php
+++ functions.php
@@ -4,3 +4,9 @@
 if (!function_exists('add_action')) return;
 
 // AJOUTER SON CODE ICI
+// https://codex.wordpress.org/fr:Th%C3%A8mes_Enfant
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+
+}
