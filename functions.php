68a411a2542f778138320fd60a7ead0139e9afbc
Mars13 <34914452+mars13code@users.noreply.github.com>
2018-01-29 01:06:49 +0100

    ajout de code pour ajouter une page admin
    https://developer.wordpress.org/reference/functions/add_theme_page/

--- functions.php
+++ functions.php
@@ -10,3 +10,21 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+function showThemePage ()
+{
+	$adminPath = __DIR__ . "/admin";
+	$adminFile = "$adminPath/index.php";
+	//if (!is_dir($adminPath)) mkdir($adminPath);
+	//if (!is_file($adminFile)) touch($adminFile);
+	
+	require_once($adminFile);
+}
+
+function addThemePage ()
+{
+	// https://developer.wordpress.org/reference/functions/add_theme_page/
+	$hookSuffix = add_theme_page('Mars13 Options', 'Mars13', 'update_themes', 'admin-mars13', 'showThemePage');
+}
+
+add_action('admin_menu', 'addThemePage');
