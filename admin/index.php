9b78c3b4f6650dd637ae648d1c961a61f1eabce0
Mars13 <34914452+mars13code@users.noreply.github.com>
2018-01-29 01:08:53 +0100

    création de la page admin
    on peut ajouter du code de dev ici (plutot que dans la partie publique)

---
+++ admin/index.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+// https://developer.wordpress.org/themes/template-files-section/page-template-files/
+$monThemeDir = __DIR__;
+$monTemplatePageDir = "$monThemeDir/page-templates";
+if (!is_dir($monTemplatePageDir))
+{
+	mkdir($monTemplatePageDir);
+}
+
+$monTemplate1 = "$monTemplatePageDir/mon-template-1.php";
+if (!is_file($monTemplate1))
+{
+	touch($monTemplate1);
+}
+$monTemplate2 = "$monTemplatePageDir/mon-template-2.php";
+if (!is_file($monTemplate2))
+{
+	touch($monTemplate2);
+}
+*/
+
+if (!function_exists('add_action')) wp_die();
+?>
+<section>
+	<h3>MARS13 OPTIONS</h3>
+</section>
